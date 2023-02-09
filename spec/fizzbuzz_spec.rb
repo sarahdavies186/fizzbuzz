790fc1716fb666a9b390fdf895cfca96224563ff
sarah <sarahdavies186@gmail.com>
2023-02-09 15:45:40 +0000

    added last tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,7 @@ require "fizzbuzz"
 
 describe "fizzbuzz" do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq "fizz"
+    expect(fizzbuzz(9)).to eq "fizz"
   end
   it 'returns "fizzbuzz" when passed 3 and 5' do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
@@ -10,7 +10,8 @@ describe "fizzbuzz" do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq "buzz"
   end
-  it 'returns "1" when passed 1' do
-    expect(fizzbuzz(1)).to eq 1
+  it "returns number when not passed 3 or 5" do
+    expect(fizzbuzz(7)).to eq 7
+    expect(fizzbuzz(11)).to eq 11
   end
 end
